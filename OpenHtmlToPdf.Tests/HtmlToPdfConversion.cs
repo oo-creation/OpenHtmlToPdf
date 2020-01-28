d6e15f7fc9d818a18f99827e4598a61e88a0b5a2
OO-Creation <oo-creation@outlook.com>
2020-01-28 12:10:55 +0100

    Added .NET Core 2.1 version

--- OpenHtmlToPdf.Tests/HtmlToPdfConversion.cs
+++ OpenHtmlToPdf.Tests/HtmlToPdfConversion.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenHtmlToPdf.Tests.Helpers;
 
 namespace OpenHtmlToPdf.Tests
